4e12d8820fed4bf43fe799c6ef92665e4fcc6585
Allef Mizael <allef.mizael@gmail.com>
2023-01-06 18:00:25 -0300

    configurando a comunicação síncrona entre o ms de pagamentos e de serviços (ao confirmar o pagamento o status no pedido também é atualizado)

--- src/main/java/br/com/appfood/pagamentos/service/PagamentoService.java
+++ src/main/java/br/com/appfood/pagamentos/service/PagamentoService.java
@@ -1,6 +1,7 @@
 package br.com.appfood.pagamentos.service;
 
 import br.com.appfood.pagamentos.dto.PagamentoDto;
+import br.com.appfood.pagamentos.http.PedidoClient;
 import br.com.appfood.pagamentos.model.Pagamento;
 import br.com.appfood.pagamentos.model.Status;
 import br.com.appfood.pagamentos.repository.PagamentoRepository;
@@ -11,6 +12,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PagamentoService {
 
@@ -18,6 +21,9 @@ public class PagamentoService {
     private PagamentoRepository repository;
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private PedidoClient pedido;
 
     public Page<PagamentoDto> obterTodos(Pageable paginacao) {
         return repository
@@ -49,6 +55,18 @@ public class PagamentoService {
 
     public void excluirPagamento(Long id) {
         repository.deleteById(id);
+    }
+
+    public void confirmarPagamento(Long id){
+        Optional<Pagamento> pagamento = repository.findById(id);
+
+        if (!pagamento.isPresent()) {
+            throw new EntityNotFoundException();
+        }
+
+        pagamento.get().setStatus(Status.CONFIRMADO);
+        repository.save(pagamento.get());
+        pedido.atualizaPagamento(pagamento.get().getPedidoId());
     }
 
 
